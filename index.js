455519f2031e4633d23df5004eb3b23b0c4cb803
Marlena Tsang <Marlena345@gmail.com>
2021-10-04 00:09:00 -0400

    commit tree 7a77b833e00f854440b401deef30549deb120366

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+//find the objects whose condition returns true
+
+function findMatching (drivers, string) {
+    return drivers.filter (function (el){
+        return el.toLowerCase().indexOf(string.toLowerCase()) !==-1
+    })
+}
+
+
+
+//function that matches first 2 letters of the name
+
+function fuzzyMatch(driver, string) {
+    let correctLetters = driver.filter(beginningLetters => beginningLetters.substring(0,2) === string)
+return correctLetters;
+}
+
+//
+
+function matchName(drivers, name){
+  return drivers.filter(function(driver){
+    return driver.name === name;
+  })
+}
